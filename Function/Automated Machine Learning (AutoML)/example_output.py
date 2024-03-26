a289a51232fe594a7cbdd6efc6755005d9a6a56a
KOSASIH <kosasihg88@gmail.com>
2024-03-26 22:22:38 +0700

    Create example_output.py

---
+++ Function/Automated Machine Learning (AutoML)/example_output.py
@@ -0,0 +1,8 @@
+Best Model: RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini',
+            max_depth=None, max_features='auto', max_leaf_nodes=None,
+            min_impurity_decrease=0.0, min_impurity_split=None,
+            min_samples_leaf=1, min_samples_split=2,
+            min_weight_fraction_leaf=0.0, n_estimators=100, n_jobs=-1,
+            oob_score=False, random_state=None, verbose=0,
+            warm_start=False)
+Best Score: 0.9866666666666667
