07b78840b0d5a0742e571e226088f2fcfca0f29d
KOSASIH <kosasihg88@gmail.com>
2024-03-26 21:01:36 +0700

    Create Simulation1.js

---
+++ Applications/NeuroForgePlay/SimulationExperiments/Simulation1.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+import Simulation1 from './Simulation1/index';
+import Simulation2 from './Simulation2/index';
+
+const SimulationExperiments = () => {
+  return (
+    <View>
+      <Simulation1 />
+      <Simulation2 />
+    </View>
+  );
+};
+
+export default SimulationExperiments;
