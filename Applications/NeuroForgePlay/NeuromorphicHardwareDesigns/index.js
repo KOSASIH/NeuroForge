018865872a317eb299bfeb67da4ae4a28c7b6495
KOSASIH <kosasihg88@gmail.com>
2024-03-26 20:58:00 +0700

    Create index.js

---
+++ Applications/NeuroForgePlay/NeuromorphicHardwareDesigns/index.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import NeuromorphicHardware1 from './NeuromorphicHardware1';
+import NeuromorphicHardware2 from './NeuromorphicHardware2';
+
+const NeuromorphicHardwareDesigns = () => {
+  return (
+    <View>
+      <NeuromorphicHardware1 />
+      <NeuromorphicHardware2 />
+    </View>
+  );
+};
+
+export default NeuromorphicHardwareDesigns;
